e4a46604a4a054c09173613f7c92c2c30b949712
amerjustice <amerjustice@outlook.com>
2016-05-12 19:49:45 -0700

    part 1

--- Binary Search/main.m
+++ Binary Search/main.m
@@ -1,17 +1,44 @@
-//
-//  main.m
-//  Binary Search
-//
-//  Created by Susan Justice on 2016-05-12.
-//  Copyright © 2016 Amer Justice. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
+
+NSMutableArray *makeArray(){
+    
+    NSMutableArray *oneM = [[NSMutableArray alloc] init];
+    
+    for (int i = 0; i < 1000000; i++){
+        [oneM addObject:[NSNumber numberWithInt:i]];
+    }
+    return oneM;
+}
+
+int searchArray(int target, NSMutableArray *array){
+    
+    NSNumber *nsTarget = [NSNumber numberWithInt:target];
+    for (int i = 0; i < [array count]; i++){
+        if([nsTarget isEqualTo:array[i]]){
+            NSLog(@"found %i",[nsTarget intValue]);
+            return [nsTarget intValue];
+        }
+    }
+    NSLog(@"could not find");
+    return -1;
+}
+
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        NSMutableArray *myArray = makeArray();
+        
+        NSDate *methodStart = [NSDate date];
+        
+        int found = searchArray(999999, myArray);
+        
+        NSDate *methodFinish = [NSDate date];
+        NSTimeInterval executionTime = [methodFinish timeIntervalSinceDate: methodStart];
+        
+        NSLog(@"executionTime = %f", executionTime);
+        
+        
     }
     return 0;
 }
