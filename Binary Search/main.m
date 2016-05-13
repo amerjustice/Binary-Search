f2661e421e79a039e4dc7ace67cbb896f05d1a76
amerjustice <amerjustice@outlook.com>
2016-05-12 19:44:18 -0700

    Initial Commit

---
+++ Binary Search/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Binary Search
+//
+//  Created by Susan Justice on 2016-05-12.
+//  Copyright © 2016 Amer Justice. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
